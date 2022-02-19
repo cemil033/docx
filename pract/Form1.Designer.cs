60fbc2d06759fdbc01eadf8211c548502cd24bc3
cemil <cemiw033@gmail.com>
2022-02-19 09:47:42 +0400

    docx

---
+++ pract/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace pract
+{
+    partial class btn_save
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb_r = new System.Windows.Forms.RadioButton();
+            this.rb_cnt = new System.Windows.Forms.RadioButton();
+            this.rb_lft = new System.Windows.Forms.RadioButton();
+            this.cb_i = new System.Windows.Forms.CheckBox();
+            this.cb_und = new System.Windows.Forms.CheckBox();
+            this.cb_bold = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.fb_savef = new System.Windows.Forms.TextBox();
+            this.tb_loadfil = new System.Windows.Forms.TextBox();
+            this.cb_color = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cb_sizes = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cb_fonts = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtb_text = new System.Windows.Forms.RichTextBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.groupBox1.Controls.Add(this.rb_r);
+            this.groupBox1.Controls.Add(this.rb_cnt);
+            this.groupBox1.Controls.Add(this.rb_lft);
+            this.groupBox1.Controls.Add(this.cb_i);
+            this.groupBox1.Controls.Add(this.cb_und);
+            this.groupBox1.Controls.Add(this.cb_bold);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btn_load);
+            this.groupBox1.Controls.Add(this.fb_savef);
+            this.groupBox1.Controls.Add(this.tb_loadfil);
+            this.groupBox1.Controls.Add(this.cb_color);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cb_sizes);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cb_fonts);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1450, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // rb_r
+            // 
+            this.rb_r.AutoSize = true;
+            this.rb_r.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rb_r.Location = new System.Drawing.Point(648, 48);
+            this.rb_r.Name = "rb_r";
+            this.rb_r.Size = new System.Drawing.Size(41, 28);
+            this.rb_r.TabIndex = 23;
+            this.rb_r.TabStop = true;
+            this.rb_r.Text = "R";
+            this.rb_r.UseVisualStyleBackColor = true;
+            this.rb_r.CheckedChanged += new System.EventHandler(this.rb_r_CheckedChanged);
+            // 
+            // rb_cnt
+            // 
+            this.rb_cnt.AutoSize = true;
+            this.rb_cnt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rb_cnt.Location = new System.Drawing.Point(610, 48);
+            this.rb_cnt.Name = "rb_cnt";
+            this.rb_cnt.Size = new System.Drawing.Size(41, 28);
+            this.rb_cnt.TabIndex = 22;
+            this.rb_cnt.TabStop = true;
+            this.rb_cnt.Text = "C";
+            this.rb_cnt.UseVisualStyleBackColor = true;
+            this.rb_cnt.CheckedChanged += new System.EventHandler(this.rb_cnt_CheckedChanged);
+            // 
+            // rb_lft
+            // 
+            this.rb_lft.AutoSize = true;
+            this.rb_lft.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rb_lft.Location = new System.Drawing.Point(573, 48);
+            this.rb_lft.Name = "rb_lft";
+            this.rb_lft.Size = new System.Drawing.Size(38, 28);
+            this.rb_lft.TabIndex = 21;
+            this.rb_lft.TabStop = true;
+            this.rb_lft.Text = "L";
+            this.rb_lft.UseVisualStyleBackColor = true;
+            this.rb_lft.CheckedChanged += new System.EventHandler(this.rb_lft_CheckedChanged);
+            // 
+            // cb_i
+            // 
+            this.cb_i.AutoSize = true;
+            this.cb_i.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cb_i.Location = new System.Drawing.Point(474, 48);
+            this.cb_i.Name = "cb_i";
+            this.cb_i.Size = new System.Drawing.Size(33, 28);
+            this.cb_i.TabIndex = 20;
+            this.cb_i.Text = "I";
+            this.cb_i.UseVisualStyleBackColor = true;
+            this.cb_i.CheckedChanged += new System.EventHandler(this.cb_i_CheckedChanged);
+            // 
+            // cb_und
+            // 
+            this.cb_und.AutoSize = true;
+            this.cb_und.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cb_und.Location = new System.Drawing.Point(426, 48);
+            this.cb_und.Name = "cb_und";
+            this.cb_und.Size = new System.Drawing.Size(42, 28);
+            this.cb_und.TabIndex = 19;
+            this.cb_und.Text = "U";
+            this.cb_und.UseVisualStyleBackColor = true;
+            this.cb_und.CheckedChanged += new System.EventHandler(this.cb_und_CheckedChanged);
+            // 
+            // cb_bold
+            // 
+            this.cb_bold.AutoSize = true;
+            this.cb_bold.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cb_bold.Location = new System.Drawing.Point(378, 48);
+            this.cb_bold.Name = "cb_bold";
+            this.cb_bold.Size = new System.Drawing.Size(42, 28);
+            this.cb_bold.TabIndex = 2;
+            this.cb_bold.Text = "B";
+            this.cb_bold.UseVisualStyleBackColor = true;
+            this.cb_bold.CheckedChanged += new System.EventHandler(this.cb_bold_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(569, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 24);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Alignment";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(1357, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btn_load
+            // 
+            this.btn_load.AutoSize = true;
+            this.btn_load.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_load.Location = new System.Drawing.Point(1357, 17);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(75, 34);
+            this.btn_load.TabIndex = 13;
+            this.btn_load.Text = "load";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // fb_savef
+            // 
+            this.fb_savef.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fb_savef.Location = new System.Drawing.Point(1047, 61);
+            this.fb_savef.Name = "fb_savef";
+            this.fb_savef.Size = new System.Drawing.Size(253, 29);
+            this.fb_savef.TabIndex = 12;
+            // 
+            // tb_loadfil
+            // 
+            this.tb_loadfil.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_loadfil.Location = new System.Drawing.Point(1047, 19);
+            this.tb_loadfil.Name = "tb_loadfil";
+            this.tb_loadfil.Size = new System.Drawing.Size(253, 29);
+            this.tb_loadfil.TabIndex = 11;
+            // 
+            // cb_color
+            // 
+            this.cb_color.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cb_color.FormattingEnabled = true;
+            this.cb_color.Location = new System.Drawing.Point(861, 44);
+            this.cb_color.Name = "cb_color";
+            this.cb_color.Size = new System.Drawing.Size(151, 32);
+            this.cb_color.TabIndex = 10;
+            this.cb_color.SelectedIndexChanged += new System.EventHandler(this.cb_color_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(897, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Colors";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(388, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Font style";
+            // 
+            // cb_sizes
+            // 
+            this.cb_sizes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cb_sizes.FormattingEnabled = true;
+            this.cb_sizes.Location = new System.Drawing.Point(265, 45);
+            this.cb_sizes.Name = "cb_sizes";
+            this.cb_sizes.Size = new System.Drawing.Size(56, 32);
+            this.cb_sizes.TabIndex = 3;
+            this.cb_sizes.SelectedIndexChanged += new System.EventHandler(this.cb_sizes_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(268, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Size";
+            // 
+            // cb_fonts
+            // 
+            this.cb_fonts.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cb_fonts.FormattingEnabled = true;
+            this.cb_fonts.Location = new System.Drawing.Point(22, 45);
+            this.cb_fonts.Name = "cb_fonts";
+            this.cb_fonts.Size = new System.Drawing.Size(151, 32);
+            this.cb_fonts.TabIndex = 1;
+            this.cb_fonts.SelectedIndexChanged += new System.EventHandler(this.cb_fonts_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(51, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Font";
+            // 
+            // rtb_text
+            // 
+            this.rtb_text.Location = new System.Drawing.Point(12, 118);
+            this.rtb_text.Name = "rtb_text";
+            this.rtb_text.Size = new System.Drawing.Size(1450, 595);
+            this.rtb_text.TabIndex = 1;
+            this.rtb_text.Text = "";
+            // 
+            // btn_save
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1474, 725);
+            this.Controls.Add(this.rtb_text);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "btn_save";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.btn_save_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cb_sizes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cb_fonts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.TextBox fb_savef;
+        private System.Windows.Forms.TextBox tb_loadfil;
+        private System.Windows.Forms.ComboBox cb_color;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox rtb_text;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.CheckBox cb_bold;
+        private System.Windows.Forms.CheckBox cb_i;
+        private System.Windows.Forms.CheckBox cb_und;
+        private System.Windows.Forms.RadioButton rb_r;
+        private System.Windows.Forms.RadioButton rb_cnt;
+        private System.Windows.Forms.RadioButton rb_lft;
+    }
+}
+
---
+++ pract/Form1.cs
@@ -0,0 +1,268 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pract
+{
+    public partial class btn_save : Form
+    {
+        public btn_save()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_save_Load(object sender, EventArgs e)
+        {
+            foreach (var item in FontFamily.Families)
+            {
+                cb_fonts.Items.Add(item.Name);
+            }
+            cb_sizes.Items.Add(8);
+            cb_sizes.Items.Add(9);
+            cb_sizes.Items.Add(10);
+            cb_sizes.Items.Add(11);
+            cb_sizes.Items.Add(12);
+            cb_sizes.Items.Add(14);
+            cb_sizes.Items.Add(16);
+            cb_sizes.Items.Add(18);
+            cb_sizes.Items.Add(20);
+            cb_sizes.Items.Add(22);
+            cb_sizes.Items.Add(24);
+            cb_sizes.Items.Add(26);
+            cb_sizes.Items.Add(28);
+            cb_sizes.Items.Add(36);
+            cb_sizes.Items.Add(48);
+            cb_sizes.Items.Add(72);
+            foreach (Color color in new ColorConverter().GetStandardValues())
+            {
+                cb_color.Items.Add(color.Name);
+            }
+            cb_bold.Appearance = System.Windows.Forms.Appearance.Button;
+            cb_i.Appearance = System.Windows.Forms.Appearance.Button;
+            cb_und.Appearance = System.Windows.Forms.Appearance.Button;
+            rb_cnt.Appearance = System.Windows.Forms.Appearance.Button;
+            rb_lft.Appearance = System.Windows.Forms.Appearance.Button;
+            rb_r.Appearance = System.Windows.Forms.Appearance.Button;
+            cb_sizes.SelectedIndex = 0;
+        }
+
+        private void cb_fonts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (rtb_text.SelectedText == null)
+            {
+                Font ExistingFont = rtb_text.SelectionFont;
+                float t = float.Parse(cb_sizes.SelectedItem.ToString());
+                rtb_text.Font = new Font(cb_fonts.SelectedItem.ToString(), t,ExistingFont.Style);
+            }
+            else
+            {
+                Font ExistingFont = rtb_text.SelectionFont;
+                float t = float.Parse(cb_sizes.SelectedItem.ToString());
+                rtb_text.SelectionFont = new Font(cb_fonts.SelectedItem.ToString(), t,ExistingFont.Style);
+            }
+        }
+        int trr=0;
+        private void cb_sizes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (rtb_text.SelectedText == null)
+            {
+                Font ExistingFont = rtb_text.SelectionFont;
+                float t = float.Parse(cb_sizes.SelectedItem.ToString());
+                rtb_text.Font = new Font(cb_fonts.SelectedItem.ToString(),t,ExistingFont.Style);
+
+            }
+            else
+            {
+                if (trr != 0)
+                {
+                    Font ExistingFont = rtb_text.SelectionFont;
+                    float t = float.Parse(cb_sizes.SelectedItem.ToString());
+                    rtb_text.SelectionFont = new Font(cb_fonts.SelectedItem.ToString(), t,ExistingFont.Style);
+                }
+                trr=1;
+            }
+        }
+        
+        private void cb_bold_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb_bold.Checked)
+            {
+                if (rtb_text.SelectedText == null)
+                {
+                    Font ExistingFont = rtb_text.Font;
+                    FontStyle NewFontStyle = new FontStyle();
+                    if (cb_bold.Checked)
+                        NewFontStyle = FontStyle.Bold;
+
+                    if (cb_und.Checked)
+                        NewFontStyle |= FontStyle.Underline;
+
+                    if (cb_i.Checked)
+                        NewFontStyle |= FontStyle.Italic;
+                    rtb_text.Font = new Font(ExistingFont, NewFontStyle);
+                }
+                else
+                {
+                    Font ExistingFont = rtb_text.SelectionFont;
+                    FontStyle NewFontStyle = new FontStyle();
+                    if (cb_bold.Checked)
+                        NewFontStyle = FontStyle.Bold;
+
+                    if (cb_und.Checked)
+                        NewFontStyle |= FontStyle.Underline;
+
+                    if (cb_i.Checked)
+                        NewFontStyle |= FontStyle.Italic;
+                    rtb_text.SelectionFont = new Font(ExistingFont, NewFontStyle);
+                }
+            }
+        }
+
+        private void cb_und_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb_und.Checked)
+            {
+                if (rtb_text.SelectedText == null)
+                {
+                    Font ExistingFont = rtb_text.Font;
+                    FontStyle NewFontStyle = new FontStyle();
+                    if (cb_bold.Checked)
+                        NewFontStyle = FontStyle.Bold;
+
+                    if (cb_und.Checked)
+                        NewFontStyle |= FontStyle.Underline;
+
+                    if (cb_i.Checked)
+                        NewFontStyle |= FontStyle.Italic;
+                    rtb_text.Font = new Font(ExistingFont, NewFontStyle);
+                }
+                else
+                {
+                    Font ExistingFont = rtb_text.SelectionFont;
+                    FontStyle NewFontStyle = new FontStyle();
+                    if (cb_bold.Checked)
+                        NewFontStyle = FontStyle.Bold;
+
+                    if (cb_und.Checked)
+                        NewFontStyle |= FontStyle.Underline;
+
+                    if (cb_i.Checked)
+                        NewFontStyle |= FontStyle.Italic;
+                    rtb_text.SelectionFont = new Font(ExistingFont, NewFontStyle);
+                }
+            }
+        }
+
+        private void cb_i_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cb_i.Checked)
+            {
+                if (rtb_text.SelectedText == null)
+                {
+                    Font ExistingFont = rtb_text.Font;
+                    FontStyle NewFontStyle = new FontStyle();
+                    if (cb_bold.Checked)
+                        NewFontStyle = FontStyle.Bold;
+
+                    if (cb_und.Checked)
+                        NewFontStyle |= FontStyle.Underline;
+
+                    if (cb_i.Checked)
+                        NewFontStyle |= FontStyle.Italic;
+                    rtb_text.Font = new Font(ExistingFont, NewFontStyle);
+                }
+                else
+                {
+                    Font ExistingFont = rtb_text.SelectionFont;
+                    FontStyle NewFontStyle = new FontStyle();
+                    if (cb_bold.Checked)
+                        NewFontStyle = FontStyle.Bold;
+
+                    if (cb_und.Checked)
+                        NewFontStyle |= FontStyle.Underline;
+
+                    if (cb_i.Checked)
+                        NewFontStyle |= FontStyle.Italic;
+                    rtb_text.SelectionFont = new Font(ExistingFont, NewFontStyle);
+                }
+            }
+        }
+
+        private void rb_lft_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rtb_text.Text!=null)
+            {
+                rtb_text.SelectAll();
+                rtb_text.SelectionAlignment = HorizontalAlignment.Left;
+                rtb_text.DeselectAll();
+            }
+        }
+
+        private void rb_cnt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rtb_text.Text != null)
+            {
+                rtb_text.SelectAll();
+                rtb_text.SelectionAlignment = HorizontalAlignment.Center;
+                rtb_text.DeselectAll();
+            }
+        }
+
+        private void rb_r_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rtb_text.Text != null)
+            {
+                rtb_text.SelectAll();
+                rtb_text.SelectionAlignment = HorizontalAlignment.Right;
+                rtb_text.DeselectAll();
+            }
+        }
+
+        private void cb_color_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (rtb_text.SelectedText == null)
+            {
+                foreach (Color color in new ColorConverter().GetStandardValues())
+                {
+                    if(color.Name == cb_color.SelectedItem.ToString())
+                    {
+                        rtb_text.ForeColor = color;
+                    }
+                }
+            }
+            else
+            {
+                foreach (Color color in new ColorConverter().GetStandardValues())
+                {
+                    if (color.Name == cb_color.SelectedItem.ToString())
+                    {
+                        rtb_text.SelectionColor = color;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            rtb_text.SaveFile(fb_savef.Text + ".docx");
+        }
+
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+            rtb_text.LoadFile(tb_loadfil.Text + ".docx");
+        }
+    }
+}
